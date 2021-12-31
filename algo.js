d9ddab5ed5d0925b26f8b193495a7d90d443cc7a
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-31 16:40:17 -0600

    add test case 3

--- algo.js
+++ algo.js
@@ -18,7 +18,7 @@ function descendingOrder(n){
 
 console.log("Test num: 12345 ||", descendingOrder(12345));
 console.log("Test num: 0 ||", descendingOrder(0));
-// console.log("Test num:  ||", descendingOrder(""));
-// console.log("Test num:  ||", descendingOrder(""));
+console.log("Test num: 42145 ||", descendingOrder(42145));
+console.log("Test num:  ||", descendingOrder());
 // console.log("Test num:  ||", descendingOrder(""));
 // console.log("Test num:  ||", descendingOrder(""));
