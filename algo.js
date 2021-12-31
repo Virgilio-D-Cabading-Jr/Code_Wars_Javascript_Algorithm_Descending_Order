6c7f5f126afe695c4f0e4435deea9506f1652b74
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-31 16:34:18 -0600

    add test case 1 and 2

--- algo.js
+++ algo.js
@@ -5,17 +5,20 @@
 function descendingOrder(n){
     let num = n.toString();
     let numList = [];
-    console.log ("Num:", num);
     for (x in num) {
         numList.push(parseInt(num[x]));
     }
     numList.sort((a, b) => b - a);
-    console.log("numList:", numList);
     n=0;
     for (let x=0; x< numList.length; x++) {
         n += numList[x] * Math.pow(10,numList.length-x-1);
-        console.log ("n:", n)
     }
+    return n;
 }
 
-descendingOrder(12345);
+console.log("Test num: 12345 ||", descendingOrder(12345));
+console.log("Test num: 0 ||", descendingOrder(0));
+// console.log("Test num:  ||", descendingOrder(""));
+// console.log("Test num:  ||", descendingOrder(""));
+// console.log("Test num:  ||", descendingOrder(""));
+// console.log("Test num:  ||", descendingOrder(""));
