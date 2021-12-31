fdeb649bf61649894105bc58e17e731758f67434
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-31 15:54:10 -0600

    load num into an array numList

--- algo.js
+++ algo.js
@@ -4,7 +4,12 @@
 
 function descendingOrder(n){
     let num = n.toString();
+    let numList = [];
     console.log ("Num:", num);
+    for (x in num) {
+        numList.push(x);
+    }
+    console.log("numList:", numList);
 }
 
 descendingOrder(12345);
