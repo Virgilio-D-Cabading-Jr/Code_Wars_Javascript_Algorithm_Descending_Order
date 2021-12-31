5dd9153c9dedb98c41d25ef5c463d03249e11586
Virgilio Cabading <virgilio.d.cabading.jr@gmail.com>
2021-12-31 16:30:22 -0600

    recreate int n in descending order of its digits

--- algo.js
+++ algo.js
@@ -7,10 +7,15 @@ function descendingOrder(n){
     let numList = [];
     console.log ("Num:", num);
     for (x in num) {
-        numList.push(parseInt(x));
+        numList.push(parseInt(num[x]));
     }
     numList.sort((a, b) => b - a);
     console.log("numList:", numList);
+    n=0;
+    for (let x=0; x< numList.length; x++) {
+        n += numList[x] * Math.pow(10,numList.length-x-1);
+        console.log ("n:", n)
+    }
 }
 
 descendingOrder(12345);
